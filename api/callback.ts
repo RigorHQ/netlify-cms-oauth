d78002f48867b75912f23ba708c86b668ad907f8
dan13ram <dan13ram@gmail.com>
2023-05-17 01:55:52 +0530

    fixed callback

--- api/callback.ts
+++ api/callback.ts
@@ -29,48 +29,46 @@ export default async (req: IncomingMessage, res: ServerResponse) => {
     res.statusCode = 200;
     res.end(responseBody);
   } catch (e) {
-    res.statusCode = 200;
-    res.end(renderBody("error", e));
+    res.statusCode = 400;
+    res.end(renderBody("error", { error: e.message, provider }));
   }
 };
 
-function renderBody(status: 'success' | 'error', content: any) {
+function renderBody(status: "success" | "error", content: any) {
   return `
   <!DOCTYPE html>
   <html lang="en">
-    <head>
-      <meta charset="utf-8">
-      <title>Authorizing ...</title>
-    </head>
-    <body>
-      <p id="message"></p>
-      <script>
-        // Output a message to the user
-        function sendMessage(message) {
-          document.getElementById("message").innerText = message;
-          document.title = message
-        }
+  <head>
+   <meta charset="utf-8">
+   <title>Authorizing ...</title>
+  </head>
+  <body>
+  <p id="message"></p>
+  <script>
+   // Output a message to the user
+   function sendMessage(message) {
+     document.getElementById("message").innerText = message;
+     document.title = message
+   }
 
-        // Handle a window message by sending the auth to the "opener"
-        function receiveMessage(message) {
-          console.debug("receiveMessage", message);
-          window.opener.postMessage(
-            'authorization:${content.provider}:${status}:${JSON.stringify(
-    content,
-  )}',
-            message.origin + '/api/oauth'
-          );
-          window.removeEventListener("message", receiveMessage, false);
-          sendMessage("Authorized, closing ...");
-        }
+   // Handle a window message by sending the auth to the "opener"
+   function receiveMessage(message) {
+     console.debug("receiveMessage", message);
+     window.opener.postMessage(
+       'authorization:${content.provider}:${status}:${JSON.stringify(content)}',
+       message.origin
+     );
+     window.removeEventListener("message", receiveMessage, false);
+     sendMessage("Authorized, closing ...");
+   }
 
-        sendMessage("Authorizing ...");
-        window.addEventListener("message", receiveMessage, false);
+   sendMessage("Authorizing ...");
+   window.addEventListener("message", receiveMessage, false);
 
-        console.debug("postMessage", "authorizing:github", "*")
-        window.opener.postMessage("authorizing:github", "*");
-      </script>
-    </body>
+   console.debug("postMessage", "authorizing:${content.provider}", "*")
+   window.opener.postMessage("authorizing:${content.provider}", "*");
+  </script>
+  </body>
   </html>
   `;
 }
